f05cf81e419bc069656425a65b72b0e6be4acb7a
nastanda1995@gmail.com <>
2014-05-14 09:14:53 +0700

    create project

---
+++ src/myfirstapp/MyFirstApp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myfirstapp;
+
+/**
+ *
+ * @author Administrator
+ */
+public class MyFirstApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic 
+        System.out.println("Hello World");
+        System.out.println("My name is Na Standa: 55026132");
+    }
+    
+}
